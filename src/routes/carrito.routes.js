f0362c4a3a7e65a1430017983223d9be6d584059
nicolas franco <gabrielfranco4321@gmail.com>
2022-12-01 21:23:38 -0300

    2

--- src/routes/carrito.routes.js
+++ src/routes/carrito.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import ContenedorArchivo from "../container/Contenedor.js";
+import ContenedorArchivo from "../container/contenedor.js";
 
 
 const routercarrito = Router();
